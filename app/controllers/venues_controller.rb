9158be51ccf77b53f4e5902bd5e98e719fb88806
Chris <cmaatouk@uchicago.edu>
2018-03-16 06:23:45 +0000

    checkbox populated

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,7 @@
 class VenuesController < ApplicationController
   def index
-    @q = Venue.ransack(params[:q])
+    @new_venue = Venue.all
+    @q = @new_venue.ransack(params[:q])
     @venues = @q.result(:distinct => true).includes(:bookmarks, :neighborhood, :fans, :specialties).page(params[:page]).per(10)
     @location_hash = Gmaps4rails.build_markers(@venues.where.not(:address_latitude => nil)) do |venue, marker|
       marker.lat venue.address_latitude
