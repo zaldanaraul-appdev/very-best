32c780b2cda54711020a486d1ccffbc855c3fdab
Chris <cmaatouk@uchicago.edu>
2018-03-16 07:06:32 +0000

    dishes/venues ordered alphabetically

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,6 +1,6 @@
 class DishesController < ApplicationController
   def index
-    @q = Dish.ransack(params[:q])
+    @q = Dish.order('name ASC').all.ransack(params[:q])
     @dishes = @q.result(:distinct => true).includes(:cuisine, :bookmarks, :fans, :specialists).page(params[:page]).per(10)
 
     render("dishes/index.html.erb")
